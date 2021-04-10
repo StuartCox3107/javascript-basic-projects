1488965dc381945e7ebbe7cfcf2c0f4e5845c03b
StuartCox3107 <60449179+StuartCox3107@users.noreply.github.com>
2021-04-10 11:12:42 +0100

    app.js consts added

--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -38,3 +38,21 @@ const reviews = [
   },
 ];
 
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
