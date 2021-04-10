75ccd730910939c428f83e239a5f95ad5eaee5cd
StuartCox3107 <60449179+StuartCox3107@users.noreply.github.com>
2021-04-10 12:19:15 +0100

    review project finished

--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -47,12 +47,37 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
-let currentItem = 0;
+let currentItem = 1;
 
 window.addEventListener("DOMContentLoaded", function(){
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+nextBtn.addEventListener("click", function() {
+  currentItem++;
+  if(currentItem > reviews.length -1) {
+    currentItem = 0;
+  };
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener("click", function() {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length -1;
+  };
+  showPerson(currentItem);
+});
+
+randomBtn.addEventListener("click", function() {
+  currentItem = Math.floor(Math.random()*reviews.length);
+  showPerson(currentItem);
 });
